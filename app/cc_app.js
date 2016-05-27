860e8e1655e8ecd6f9a391fffd8934fe62bffff8
somethingsaid <kwong.billy@gmail.com>
2016-05-27 13:11:15 -0400

    Countries listing working

--- app/cc_app.js
+++ app/cc_app.js
@@ -12,4 +12,25 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate'])
 		template: '<p>Error - Page Not Found</p>'
 	})
 	.otherwise('/error');
+}])
+.controller('homeCtrl', ['$scope', function($scope) {
+	// empty for now
+}])
+.controller('countriesCtrl', ['$http', '$scope', function($http, $scope) {
+	var params = {
+		username: 'bckwong',
+		lang: 'en',
+		type: 'JSON'
+	};
+  $http({
+  	url: 'http://api.geonames.org/countryInfo?',
+  	method: 'GET',
+  	params: params
+  })
+  .then(function(response) {
+  	$scope.countries = response.data.geonames;
+  	console.log($scope.countries);
+  }, function(response) {
+  	console.log('Something is wrong');
+  });
 }]);
