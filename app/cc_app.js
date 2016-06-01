e4330d5dd9828ecf0af86612780cd506cfe0c7f7
somethingsaid <kwong.billy@gmail.com>
2016-06-01 14:43:14 -0400

    Trying to retrieve country-specific and capital data only

--- app/cc_app.js
+++ app/cc_app.js
@@ -1,4 +1,5 @@
 angular.module('ccApp', ['ngRoute', 'ngAnimate'])
+.constant("countryListParams", {username: 'bckwong', lang: 'en', type: 'JSON'})
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl: './home/home.html',
@@ -26,16 +27,13 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate'])
 .controller('homeCtrl', ['$scope', function($scope) {
 	// empty for now
 }])
-.controller('countriesCtrl', ['$http', '$scope', function($http, $scope) {
-	var params = {
-		username: 'bckwong',
-		lang: 'en',
-		type: 'JSON'
-	};
+.controller('countriesCtrl', ['$http', '$scope', 'countryListParams', function($http, $scope, countryListParams) {
+	console.log(countryListParams);
+	// Making a call to return high level information on all available countries
   $http({
   	url: 'http://api.geonames.org/countryInfo?',
   	method: 'GET',
-  	params: params
+  	params: countryListParams
   })
   .then(function(response) {
   	$scope.countries = response.data.geonames;
@@ -44,6 +42,21 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate'])
   	console.log('Something is wrong');
   });
 }])
-.controller('countryCodeCtrl', ['$http', '$scope', 'countryCode', function($http, $scope, countryCode) {
-	$scope.countryCode = countryCode;
+.controller('countryCodeCtrl', ['$http', '$scope', 'countryCode', 'countryListParams', function($http, $scope, countryCode, countryListParams) {
+	// Appending country to parameters for specific search
+	var countryParams = countryListParams;
+	countryParams.country = countryCode;
+	console.log("Country specific parameters: " + JSON.stringify(countryParams));
+	console.log("Original parameters for all countries: " + JSON.stringify(countryListParams));
+	$http({
+		url: 'http://api.geonames.org/countryInfo?',
+  	method: 'GET',
+  	params: $scope.countryParams
+	})
+	.then(function(response) {
+		$scope.country = response.data.geonames;
+		console.log($scope.country);
+	}, function(response) {
+		console.log('Something is wrong');
+	})
 }]);
