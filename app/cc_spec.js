3b8208b1aaff143d987552a016fa7d8ea26b341b
somethingsaid <kwong.billy@gmail.com>
2016-06-14 11:11:13 -0400

    added mocks

---
+++ app/cc_spec.js
@@ -0,0 +1,18 @@
+// Unit Testing the getCountryData() service
+describe("getCountryData", function() {
+	beforeEach(module("ccLibrary"));
+
+	it("should return a response object when called",
+	inject(function(getCountryData, $rootScope, $httpBackend, Params) {
+		$httpBackend.expect('GET', 'http://api.geonames.org/countryInfo?').respond(200);
+		var status = false;
+		var testParams = new Params();
+		getCountryData(null, testParams).then(function() {
+			status = true;
+		});
+		$rootScope.$digest();
+    $httpBackend.flush();
+    expect(status).toBe(true);
+    $httpBackend.verifyNoOutstandingRequest();
+	}));
+});
